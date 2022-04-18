e1590801372aa1fa8b585ea4790d4e5a688f14ff
KindaOP <connettotyta@gmail.com>
2022-04-18 09:18:35 +0700

    added topbar

---
+++ index.js
@@ -0,0 +1,41 @@
+const DEBUG = true;
+const MAX_BOOKMARK = 3;
+
+
+function toggleSidebar() {
+    const sidebar = document.getElementsByClassName("MainSidebar")[0];
+    let status = sidebar.getAttribute("active");
+    status = status==="true"? "false":"true";
+    sidebar.setAttribute("active", status);
+};
+
+
+function openLink(link) {
+    window.open(link, '_blank');
+};
+
+
+function checkBookmark() {
+    let bookmark = document.getElementsByClassName("Bookmark")[0];
+    let bookmarkButton = document.getElementBy 
+    const bookmarkArray = bookmark.getAttribute("bookmarked");
+    const currentPage = document.getElementById("currentPage").innerHTML;
+    const indexToRemove = bookmarkArray.indexOf(currentPage);
+    if (indexToRemove > -1) {
+        bookmarkArray.splice(indexToRemove, 1);
+    } 
+    else {
+        bookmarkArray.push(currentPage)
+        if (bookmarkArray.length>MAX_BOOKMARK) {
+            bookmarkArray.shift();
+        }
+    }
+    bookmark.setAttribute("bookmarked", bookmarkArray);
+    bookmark.innerHTML = "";
+    for (const page of bookmarkArray) {
+        let pageButton = document.createElement('div');
+        pageButton.setAttribute("class", "PageButton")
+        pageButton.setAttribute("id", page);
+        bookmark.appendChild(pageButton);
+    }
+};
