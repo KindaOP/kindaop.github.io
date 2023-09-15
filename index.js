484f8fe372ff00c237821de1e6a6589b6f7b4a74
KindaOP <connettotyta@gmail.com>
2023-09-16 04:21:06 +0700

    Reworked overall website.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const DEBUG = true;
-const MAX_BOOKMARK = 3;
 
 
 function toggleSidebar() {
-    const sidebar = document.getElementsByClassName("MainSidebar")[0];
+    const sidebar = document.getElementById("MainBar").getElementsByClassName("Sidebar")[0];
     let status = sidebar.getAttribute("active");
     status = status==="true"? "false":"true";
     sidebar.setAttribute("active", status);
@@ -12,30 +11,4 @@ function toggleSidebar() {
 
 function openLink(link) {
     window.open(link, '_blank');
-};
-
-
-const bookmarkArray = [];
-
-function checkBookmark() {
-    const bookmark = document.getElementsByClassName("Bookmark")[0];
-    const currentPage = document.getElementById("currentPage").innerHTML;
-    const indexToRemove = bookmarkArray.indexOf(currentPage);
-    if (indexToRemove > -1) {
-        bookmarkArray.splice(indexToRemove, 1);
-    } 
-    else {
-        bookmarkArray.push(currentPage)
-        if (bookmarkArray.length>MAX_BOOKMARK) {
-            bookmarkArray.shift();
-        }
-    }
-    bookmark.setAttribute("bookmarked", bookmarkArray);
-    bookmark.innerHTML = "";
-    for (const page of bookmarkArray) {
-        let pageButton = document.createElement('div');
-        pageButton.setAttribute("class", "PageButton")
-        pageButton.setAttribute("id", page);
-        bookmark.appendChild(pageButton);
-    }
 };
